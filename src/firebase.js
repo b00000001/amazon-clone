a101993107247fb068f80613fe94fabebff86143
Aaron <deas.aaron@gmail.com>
2021-04-07 02:15:38 -0400

    Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyApFTwnVdkI0dRA97fDmiVtCwsDo1pZ1Vs",
+  authDomain: "clone-a5751.firebaseapp.com",
+  projectId: "clone-a5751",
+  storageBucket: "clone-a5751.appspot.com",
+  messagingSenderId: "844713830094",
+  appId: "1:844713830094:web:ecb3723a78d40ca6e9d29f",
+};
