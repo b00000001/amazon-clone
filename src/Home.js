2d27c831fd3d5aa4109b21c98f09facb9a549a37
Aaron <deas.aaron@gmail.com>
2021-04-08 02:02:28 -0400

    Commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZWExM2NmYjct/ZWExM2NmYjct-ZTk5ZjdkNzkt-w3000._CB655980992_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
