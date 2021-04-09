e5494a71be2f3f5e368f6b992cea93173acdbb6b
Aaron <deas.aaron@gmail.com>
2021-04-09 02:22:57 -0400

    Commit

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
     <div className="home">
       <div className="home__container">
         <img
+          className="home__image"
           src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZWExM2NmYjct/ZWExM2NmYjct-ZTk5ZjdkNzkt-w3000._CB655980992_.jpg"
           alt=""
         />
+
+        <div className="home__row">
+          <Product />
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">{/* Product */}</div>
       </div>
     </div>
   );
