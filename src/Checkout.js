21f3cc4340ae789d6c0d369418b5990fe4dc08e9
Aaron <deas.aaron@gmail.com>
2021-04-10 21:15:05 -0400

    Commit

---
+++ src/Checkout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+import { useStateValue } from "./stateprovider";
+import CheckoutProduct from "./CheckoutProduct";
+
+function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+          className="checkout__ad"
+        />
+
+        <div className="checkout__title">
+          <h2>Your shopping basket</h2>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+          {/* Checkout Product */}
+          {/* Checkout Product */}
+          {/* Checkout Product */}
+          {/* Checkout Product */}
+          {/* Checkout Product */}
+        </div>
+      </div>
+
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
